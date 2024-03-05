aa4c8f50e756d66f11c83da54f013db7a28f5c0c
Rafaela3613 <rafaelarabelobarbosa@outlook.com>
2024-03-05 10:45:40 -0300

    criando o Título

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -1,12 +1,16 @@
 import Banner from "componentes/Banner";
 import Cabecalho from "componentes/Cabecalho";
 import Rodape from "componentes/Rodape";
+import Titulo from "componentes/Titulo";
 
 function Inicio() {
     return(
         <>
         <Cabecalho />
         <Banner imagem="home" />
+        <Titulo>
+            <h1>Um lugar para guardar seus vídeos e filmes!</h1>
+        </Titulo>
         <Rodape />
         </>
     )
