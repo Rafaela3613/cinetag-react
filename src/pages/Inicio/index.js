4458f7224a1271f771c92dc05ca28f8624b6fae5
Rafaela3613 <rafaelarabelobarbosa@outlook.com>
2024-03-06 15:07:40 -0300

    criando os cards

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -1,17 +1,19 @@
 import Banner from "componentes/Banner";
 import Cabecalho from "componentes/Cabecalho";
+import Card from "componentes/Card";
 import Rodape from "componentes/Rodape";
 import Titulo from "componentes/Titulo";
 
 function Inicio() {
-    return(
+    return (
         <>
-        <Cabecalho />
-        <Banner imagem="home" />
-        <Titulo>
-            <h1>Um lugar para guardar seus vídeos e filmes!</h1>
-        </Titulo>
-        <Rodape />
+            <Cabecalho />
+            <Banner imagem="home" />
+            <Titulo>
+                <h1>Um lugar para guardar seus vídeos e filmes!</h1>
+            </Titulo>
+            <Card id='1' titulo='Gato bonifácio' capa='https://thecatapi.com/api/images/get?format-src&type=png' />
+            <Rodape />
         </>
     )
 }
