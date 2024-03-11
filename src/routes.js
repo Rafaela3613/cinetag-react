8853b448b40404590d6431e139caf5201af6b48c
Rafaela3613 <rafaelarabelobarbosa@outlook.com>
2024-03-11 09:22:00 -0300

    criando a pasta favoritos

--- src/routes.js
+++ src/routes.js
@@ -1,3 +1,6 @@
+import Cabecalho from "components/Cabecalho";
+import Container from "components/Container";
+import Rodape from "components/Rodape";
 import Favoritos from "pages/Favoritos";
 import Inicio from "pages/Inicio";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
@@ -5,12 +8,16 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 function AppRoutes() {
     return (
         <BrowserRouter>
+            <Cabecalho />
+            <Container>
                 <Routes>
                     <Route path="/" element={<Inicio />}></Route>
                     <Route path="/favoritos" element={<Favoritos />}></Route>
                 </Routes>
+            </Container>
+            <Rodape />
         </BrowserRouter>
     )
 }
 
-export default AppRoutes; 
+export default AppRoutes;
